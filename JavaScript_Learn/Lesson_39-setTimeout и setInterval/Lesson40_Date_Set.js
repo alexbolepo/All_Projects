016a77d3e5e4a93c16a70690a42e38704074aff6
Lesha <akrav8@yahoo.com>
2021-05-08 17:56:06 +0300

    Lesson 41

---
+++ JavaScript_Learn/Lesson_39-setTimeout и setInterval/Lesson40_Date_Set.js
@@ -0,0 +1,25 @@
+console.log(`Today is : ${Date()} milices`);
+// const now = new Date('2121-04-14');
+// const now = new Date(0);
+// console.log(now);
+// //new Date.parse('2121-04-14');
+
+// console.log(now.setHours(18));
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+
+// console.log(now.getUTCHours());
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+let start = new Date();
+for (let index = 0; index < 10; index++) {
+    //const element = array[index];
+    const element =  index ** 3 ;
+    console.log(element);
+}
+let end = new Date();
+console.log(`Its take time (This cicle worked "Interpolation"): ${end-start} milices`);
